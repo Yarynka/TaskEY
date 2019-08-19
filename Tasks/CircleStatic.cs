4827754bcd27a83687331deb75f26e65f7b78464
Yarynka <yarinkasirko@gmail.com>
2019-08-20 00:33:20 +0400

    Add Task 1.4. Circle

---
+++ Tasks/CircleStatic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class CircleStatic
+    {
+        const double Pi = 3.14;
+        public static double C(double r)
+        {
+            return 2 * Pi * r;
+        }
+        public static double S(double r)
+        {
+            return Pi * r * r;
+        }
+    }
+}
