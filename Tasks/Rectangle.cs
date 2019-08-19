0d3f99d8d92b578f547d2d0cf510b254521469e2
Yarynka <yarinkasirko@gmail.com>
2019-08-20 00:12:27 +0400

    Add Task1

---
+++ Tasks/Rectangle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Rectangle
+    {
+        public double xl;
+        public double xr;
+        public double yl;
+        public double yr;
+
+        public Rectangle(double xl, double yl, double xr, double yr)
+        {
+            this.xl = xl;
+            this.xr = xr;
+            this.yl = yl;
+            this.yr = yr;
+        }
+
+        public double a(double xl, double xr)
+        {
+            if (xl == xr)
+            {
+                Console.WriteLine("Прямокутника не існує:");
+            }
+            double a = xl - xr;
+            if (a < 0) return a * (-1);
+            else return a;
+
+        }
+
+        public double b(double yl, double yr)
+        {
+            if (yl == yr)
+            {
+                Console.WriteLine("Прямокутника не існує:");
+            }
+            double b = yl - yr;
+            if (b < 0) return b * (-1);
+            else return b;
+
+        }
+
+        public double Perimeter_rectangle()
+        {
+
+            double aa = a(xr, xl);
+            double bb = b(yr, yl);
+            return 2 * (aa + bb);
+        }
+        public double Square_rectangle()
+        {
+            double aa = a(xr, xl);
+            double bb = b(yr, yl);
+            return aa * bb;
+        }
+    }
+}
