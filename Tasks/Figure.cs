2ed2084c0548f8a027487d6a3f55448c089b0057
Yarynka <yarinkasirko@gmail.com>
2019-08-20 02:12:23 +0400

    завдання 1 task 2 створення базового класу..

---
+++ Tasks/Figure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    abstract class Figure
+    {
+        public abstract void Draw();
+        
+    }
+
+    class Square : Figure
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Square");
+        }
+    }
+    class Rectangles : Figure
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Rectangle");
+        }
+    }
+}
