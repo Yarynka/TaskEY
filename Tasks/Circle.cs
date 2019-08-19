4e0a43f0ee59bb8cb7dedeb0944284bed8e432c1
Yarynka <yarinkasirko@gmail.com>
2019-08-20 00:22:53 +0400

    add task1.3

---
+++ Tasks/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Circle
+    {
+        const double Pi = 3.14;
+        public double C(double r)
+        {
+            return 2 * Pi * r;
+        }
+        public double S(double r)
+        {
+            return Pi * r * r;
+        }
+    }
+}
