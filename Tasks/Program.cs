f6f20e5f74cc5386b84c36f18b288b161d6f4ce9
Yarynka <yarinkasirko@gmail.com>
2019-08-27 15:24:30 +0400

    Task 3.1 вивести імя та вік

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -10,106 +10,112 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
-            //TASK1111111111111111111111111111111
+            /*  //TASK1111111111111111111111111111111
 
-            Console.WriteLine("ЗАВДАННЯ 1.1:");
-            Console.WriteLine("Координати лiвого верхнього кута:");
-            Console.WriteLine("XL = ");
-            double xl = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("YL =");
-            double yl = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Координати правого верхнього кута:");
-            Console.WriteLine("XR =");
-            double xr = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("YR =");
-            double yr = Convert.ToDouble(Console.ReadLine());
-            
-            //Завдання 1
-            Rectangle Rectangle1 = new Rectangle(xl, yl, xr, yr);
-            Console.WriteLine("Периметр прямокутника: " + Rectangle1.Perimeter_rectangle());
-            Console.WriteLine("Площа прямокутника: " + Rectangle1.Square_rectangle());
+              Console.WriteLine("ЗАВДАННЯ 1.1:");
+              Console.WriteLine("Координати лiвого верхнього кута:");
+              Console.WriteLine("XL = ");
+              double xl = Convert.ToDouble(Console.ReadLine());
+              Console.WriteLine("YL =");
+              double yl = Convert.ToDouble(Console.ReadLine());
+              Console.WriteLine("Координати правого верхнього кута:");
+              Console.WriteLine("XR =");
+              double xr = Convert.ToDouble(Console.ReadLine());
+              Console.WriteLine("YR =");
+              double yr = Convert.ToDouble(Console.ReadLine());
 
-            //Завдання 2
+              //Завдання 1
+              Rectangle Rectangle1 = new Rectangle(xl, yl, xr, yr);
+              Console.WriteLine("Периметр прямокутника: " + Rectangle1.Perimeter_rectangle());
+              Console.WriteLine("Площа прямокутника: " + Rectangle1.Square_rectangle());
 
-            Console.WriteLine(" ");
-            Console.WriteLine("ЗАВДАННЯ 1.2:");
-            RectangleAuto Rectangle2 = new RectangleAuto();
-            Rectangle2.xl = xl;
-            Rectangle2.yl = yl;
-            Rectangle2.xr = xr;
-            Rectangle2.yr = yr;
-            Rectangle2.Reactangle_A();
-            Console.WriteLine("Периметр прямокутника: " + Rectangle2.P_rect);
-            Console.WriteLine("Площа прямокутника: " + Rectangle2.S_rect);
+              //Завдання 2
 
-            //Завдання 3
+              Console.WriteLine(" ");
+              Console.WriteLine("ЗАВДАННЯ 1.2:");
+              RectangleAuto Rectangle2 = new RectangleAuto();
+              Rectangle2.xl = xl;
+              Rectangle2.yl = yl;
+              Rectangle2.xr = xr;
+              Rectangle2.yr = yr;
+              Rectangle2.Reactangle_A();
+              Console.WriteLine("Периметр прямокутника: " + Rectangle2.P_rect);
+              Console.WriteLine("Площа прямокутника: " + Rectangle2.S_rect);
 
-            Console.WriteLine("");
-            Console.WriteLine("ЗАВДАННЯ 1.3");
-            Console.WriteLine("R = ");
-            double R = Convert.ToDouble(Console.ReadLine());
+              //Завдання 3
 
-            Circle Circle1 = new Circle();
-            Console.WriteLine("Довжнина кола: " + Circle1.C(R));
-            Console.WriteLine("Площа кола: " + Circle1.S(R));
-            //Завдання 4
+              Console.WriteLine("");
+              Console.WriteLine("ЗАВДАННЯ 1.3");
+              Console.WriteLine("R = ");
+              double R = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("");
-            Console.WriteLine("ЗAВДАННЯ 1.4");
+              Circle Circle1 = new Circle();
+              Console.WriteLine("Довжнина кола: " + Circle1.C(R));
+              Console.WriteLine("Площа кола: " + Circle1.S(R));
+              //Завдання 4
 
-            Console.WriteLine("Периметр прямокутника: " + RectangleStatic.Perimeter_rectangle(xl,yl,xr,yr));
-            Console.WriteLine("Площа прямокутника: " + RectangleStatic.Square_rectangle(xl, yl, xr, yr));
+              Console.WriteLine("");
+              Console.WriteLine("ЗAВДАННЯ 1.4");
 
-            Console.WriteLine("Довжнина кола: " + CircleStatic.C(R));
-            Console.WriteLine("Площа кола: " + CircleStatic.S(R));
-            
-            //Завдання 5
-            Console.WriteLine("");
-            Console.WriteLine("ЗAВДАННЯ 1.5");
-            
-            Console.WriteLine("а(дiйсне) = ");
-            double a_r = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("b(уявне) = ");
-            double b_im = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("c(дiйсне) = ");
-            ComplexNumber a = new ComplexNumber(a_r, b_im);
-            double c_r = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("d(уявне) = ");
-            double d_im = Convert.ToDouble(Console.ReadLine());
-            ComplexNumber b = new ComplexNumber(c_r, d_im);
+              Console.WriteLine("Периметр прямокутника: " + RectangleStatic.Perimeter_rectangle(xl,yl,xr,yr));
+              Console.WriteLine("Площа прямокутника: " + RectangleStatic.Square_rectangle(xl, yl, xr, yr));
 
-            ComplexNumber mul = a * b;
-            ComplexNumber div = a / b;
-            Console.WriteLine("(" + a.real_number + " + " + a.imaginary_number + "i) * (" + b.real_number + " + " + b.imaginary_number + "i) = (" + mul.real_number + " + " + mul.imaginary_number + "i)");
-            Console.WriteLine("(" + a.real_number + " + " + a.imaginary_number + "i) / (" + b.real_number + " + " + b.imaginary_number + "i) = (" + Math.Round(div.real_number, 2) + " + " + Math.Round(div.imaginary_number, 2) + "i)");
+              Console.WriteLine("Довжнина кола: " + CircleStatic.C(R));
+              Console.WriteLine("Площа кола: " + CircleStatic.S(R));
+
+              //Завдання 5
+              Console.WriteLine("");
+              Console.WriteLine("ЗAВДАННЯ 1.5");
+
+              Console.WriteLine("а(дiйсне) = ");
+              double a_r = Convert.ToDouble(Console.ReadLine());
+              Console.WriteLine("b(уявне) = ");
+              double b_im = Convert.ToDouble(Console.ReadLine());
+              Console.WriteLine("c(дiйсне) = ");
+              ComplexNumber a = new ComplexNumber(a_r, b_im);
+              double c_r = Convert.ToDouble(Console.ReadLine());
+              Console.WriteLine("d(уявне) = ");
+              double d_im = Convert.ToDouble(Console.ReadLine());
+              ComplexNumber b = new ComplexNumber(c_r, d_im);
+
+              ComplexNumber mul = a * b;
+              ComplexNumber div = a / b;
+              Console.WriteLine("(" + a.real_number + " + " + a.imaginary_number + "i) * (" + b.real_number + " + " + b.imaginary_number + "i) = (" + mul.real_number + " + " + mul.imaginary_number + "i)");
+              Console.WriteLine("(" + a.real_number + " + " + a.imaginary_number + "i) / (" + b.real_number + " + " + b.imaginary_number + "i) = (" + Math.Round(div.real_number, 2) + " + " + Math.Round(div.imaginary_number, 2) + "i)");
+              Console.ReadLine();
+
+
+              //завдання 2.1 реалізоване в Figure.cs або 2ed2084c0548f8a027487d6a3f55448c089b0057
+              //завдання 2.2 реалізоване в Figure.cs або 42bc32f2f8257565e1fa378902b08bc6cb7b7b2e
+              Console.WriteLine("ЗAВДАННЯ 2.1,2.2");
+
+              Tasks.Rectangles RR = new Tasks.Rectangles(1, 2);
+              RR.Draw();
+              Tasks.Square SS = new Tasks.Square(1, 2);
+              SS.Draw();
+
+              // завдання 2.3. реалізоване в CFigure.cs або  a549b7569dd0586926e59cd1759ab1faacdbb75f          
+              Console.WriteLine("ЗAВДАННЯ 2.3");
+              Tasks.CFigure F = new Tasks.CFigure(1, 2);
+               F.Draw();
+               Tasks.CRectangles R3 = new Tasks.CRectangles(1, 2);
+               R3.Draw();
+               Tasks.CSquare S3 = new Tasks.CSquare(1, 2);
+               S3.Draw();
+              //завдання 2.4. реалізоване в NFigure.cs
+              Tasks.IDrawable[] ID = new Tasks.IDrawable[3];
+              ID[0] = new Tasks.NFigure(1, 2);
+              ID[1] = new Tasks.NRectangles(1, 2);
+              ID[2] = new Tasks.NSquare(1, 2);
+              DrawAll(ID);
+              */
+            //завданння 3.1.Вивести список персон(імя,вік)
+            Console.WriteLine("ЗAВДАННЯ 3.1: ");
+            Task3_Person myPerson3_1 = new Task3_Person();
+            Console.WriteLine(myPerson3_1.PersonNameAndAge());
             Console.ReadLine();
-
-
-            //завдання 2.1 реалізоване в Figure.cs або 2ed2084c0548f8a027487d6a3f55448c089b0057
-            //завдання 2.2 реалізоване в Figure.cs або 42bc32f2f8257565e1fa378902b08bc6cb7b7b2e
-            Console.WriteLine("ЗAВДАННЯ 2.1,2.2");
-
-            Tasks.Rectangles RR = new Tasks.Rectangles(1, 2);
-            RR.Draw();
-            Tasks.Square SS = new Tasks.Square(1, 2);
-            SS.Draw();
-
-            // завдання 2.3. реалізоване в CFigure.cs або  a549b7569dd0586926e59cd1759ab1faacdbb75f          
-            Console.WriteLine("ЗAВДАННЯ 2.3");
-            Tasks.CFigure F = new Tasks.CFigure(1, 2);
-             F.Draw();
-             Tasks.CRectangles R3 = new Tasks.CRectangles(1, 2);
-             R3.Draw();
-             Tasks.CSquare S3 = new Tasks.CSquare(1, 2);
-             S3.Draw();
-            //завдання 2.4. реалізоване в NFigure.cs
-            Tasks.IDrawable[] ID = new Tasks.IDrawable[3];
-            ID[0] = new Tasks.NFigure(1, 2);
-            ID[1] = new Tasks.NRectangles(1, 2);
-            ID[2] = new Tasks.NSquare(1, 2);
-            DrawAll(ID);
-
-
+            //завданння 3.2.Вивести список персон(імя,вік)
+            //завданння 3.3.Вивести список персон(імя,вік)
         }
         public static void DrawAll(Tasks.IDrawable[] array)
         {
---
+++ Tasks/Task3_Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public int PhoneNumber;
+
+        public Person(string Name, int Age, int PhoneNumber)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.PhoneNumber = PhoneNumber;
+        }
+    }
+
+    class Task3_Person
+    {
+        List<Person> myPerson = null;
+        public Task3_Person()
+        {
+             myPerson = new List<Person>{
+                new Person("Lilia",24, 0942751986),
+                new Person("Katya",35, 0875494683),
+                new Person ("Anna", 20, 0504976852),
+                new Person ("Pavlo",18, 0579468215),
+                new Person ("John", 44, 0557689451),
+                new Person ("Olha", 29, 0675489762),
+                new Person ("Daryna",34, 0978465982)
+            };
+        }
+        public string PersonNameAndAge()
+        {
+            string result=" ";
+            foreach (Person person in myPerson)
+            {
+                result += "Name:" + person.Name + ";  Age:" + person.Age + ";\n"; 
+            }
+            return result;
+        }
+
+
+    }
+
+
+
+}
+
+
+
